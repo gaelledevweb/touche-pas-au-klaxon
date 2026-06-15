0f0a05a9ba0eaa9c6f453fa0d343b185d627db4b
GaelleDevWeb72 <gaelle.devweb72@gmail.com>
2026-06-15 17:03:13 +0200

    verification

--- test.php
+++
@@ -1,6 +0,0 @@
-<?php
-if (file_exists('includes/header.php')) {
-    echo "Le fichier est bien trouvé !";
-} else {
-    echo "Le fichier n'est pas trouvé. Vérifie le nom du dossier et du fichier.";
-}
